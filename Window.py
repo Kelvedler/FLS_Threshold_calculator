eb5eebf6b259a5341f8ed5cf1c12f88218985829
Yaroslav <hnflkmvjcm@gmail.com>
2020-09-05 17:10:37 +0300

    added data entry box

--- Window.py
+++ Window.py
@@ -1,5 +1,4 @@
 from tkinter import *
-from PIL import ImageTk, Image
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
 
@@ -8,7 +7,10 @@ root.title("FLS Threshold Calculator")
 root.geometry("800x800")
 
 frame = LabelFrame(root, padx=5, pady=5)
-frame.grid(padx=10, pady=10)
+frame.grid(row=0, column=0, padx=10, pady=10)
+
+entry_frame = LabelFrame(root, padx=5, pady=5)
+entry_frame.grid(row=1, column=0, padx=10, pady=10, sticky=W)
 
 fls_active = 2
 graph_active = Label(frame)
@@ -19,12 +21,17 @@ def graph_background_create():
     global graph_active
     if fls_active == 1:
         graph_75x100 = Figure(figsize=(6, 5), dpi=100)
-        graph_75x100.add_subplot(111).plot([0, 45, 100, 100], [75, 75, 30, 0], "-k")
+        plot = graph_75x100.add_subplot(111)
+        plot.plot([0, 45, 100, 100], [75, 75, 30, 0], "-k")
+        plot.plot([0, 50], [0, 30], "-r")
+
         graph_active = FigureCanvasTkAgg(graph_75x100, master=frame)
         graph_active.draw()
     else:
         graph_56x75 = Figure(figsize=(6, 5), dpi=100)
-        graph_56x75.add_subplot(111).plot([0, 33, 75, 75], [56, 56, 22, 0], "-k")
+        plot = graph_56x75.add_subplot(111)
+        plot.plot([0, 33, 75, 75], [56, 56, 22, 0], "-k")
+        plot.plot([0, 50], [0, 30], "-r")
 
         graph_active = FigureCanvasTkAgg(graph_56x75, master=frame)
         graph_active.draw()
@@ -48,13 +55,31 @@ def open_data_win():
     vert_slider.grid(row=0, column=1)
 
 
-data_win_op_btn = Button(root, text="Data table", command=open_data_win)
-data_win_op_btn.grid(row=1, column=0)
+graph_background_create()
 
-graph_switch_btn = Button(frame, text="Change Graph", command=graph_switch)
+graph_switch_btn = Button(frame, text="Change Graph", width=12, command=graph_switch)
 graph_switch_btn.grid(row=1, column=0)
 
-graph_background_create()
+ac_label = Label(entry_frame, text="Registration\nNumber").grid(row=0, column=0)
+ac_entry = Entry(entry_frame, width=10).grid(row=1, column=0)
+
+fh_label = Label(entry_frame, text="Flight Hours\n ").grid(row=0, column=1)
+fh_entry = Entry(entry_frame, width=10).grid(row=1, column=1)
+
+fc_label = Label(entry_frame, text="Flight Cycles\n ").grid(row=0, column=2)
+fc_entry = Entry(entry_frame, width=10).grid(row=1, column=2)
+
+fh_daily_label = Label(entry_frame, text="Flight Hours\nDaily").grid(row=0, column=3)
+fh_daily_entry = Entry(entry_frame, width=10).grid(row=1, column=3)
+
+fc_daily_label = Label(entry_frame, text="Flight Cycles\nDaily").grid(row=0, column=4)
+fc_daily_entry = Entry(entry_frame, width=10).grid(row=1, column=4)
+
+save_btn = Button(entry_frame, text="Save", command=None, width=12)
+save_btn.grid(row=1, column=5)
+
+data_win_op_btn = Button(root, text="Data table", width=12, command=open_data_win)
+data_win_op_btn.grid(row=3, column=0, padx=10, pady=10, sticky=W)
 
 
 root.mainloop()
