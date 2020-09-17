efce4655350ef2fc6727466b75c77ba7923a8793
Yaroslav <hnflkmvjcm@gmail.com>
2020-09-17 15:17:57 +0300

    add functioning graph

--- Window.py
+++ Window.py
@@ -10,6 +10,7 @@ frame = LabelFrame(root, padx=5, pady=5)
 frame.grid(row=0, column=0, padx=10, pady=10)
 
 fls_active = 2
+input_info = []
 graph_active = Label(frame)
 graph_active.grid(row=0, column=0, padx=20, pady=20)
 
@@ -56,19 +57,19 @@ def input_box():
 
     def save_info():
         if data_check("save"):
-            input_info = [reg_num.get(),
-                          flight_h.get(),
+            input_info = [flight_h.get(),
                           flight_c.get(),
                           flight_h_daily.get(),
                           flight_c_daily.get()]
 
     def draw_graph():
         if data_check("draw"):
-            input_info = [reg_num.get(),
-                          flight_h.get(),
-                          flight_c.get(),
-                          flight_h_daily.get(),
-                          flight_c_daily.get()]
+            global input_info
+            input_info = [float(flight_h.get()),
+                          float(flight_c.get()),
+                          float(flight_h_daily.get()),
+                          float(flight_c_daily.get())]
+            graph_background_create()
 
     entry_frame = LabelFrame(root, padx=5, pady=5)
     entry_frame.grid(row=1, column=0, padx=10, pady=10, sticky=W)
@@ -109,25 +110,76 @@ def input_box():
     draw_btn.grid(row=1, column=6)
 
 
+def intersection_point():
+    fl_h, fl_c, fl_h_d, fl_c_d = input_info
+    floating_line = ((fl_h, fl_c), (fl_h + fl_h_d, fl_c + fl_c_d))
+
+    def intersection_calc(line1, line2):
+        xdiff = (line1[0][0] - line1[1][0], line2[0][0] - line2[1][0])
+        ydiff = (line1[0][1] - line1[1][1], line2[0][1] - line2[1][1])
+
+        def det(a, b):
+            return a[0] * b[1] - a[1] * b[0]
+
+        div = det(xdiff, ydiff)
+        if div == 0:
+            raise Exception('no intersection')
+        d = (det(*line1), det(*line2))
+        x = det(d, xdiff) / div
+        y = det(d, ydiff) / div
+        return x, y
+    if fls_active == 1:
+        threshold_lines = [((0, 75000), (45000, 75000)),
+                           ((45000, 75000), (100000, 30000)),
+                           ((100000, 30000), (100000, 0))]
+    else:
+        threshold_lines = [((0, 56000), (33000, 56000)),
+                           ((33000, 56000), (75000, 22000)),
+                           ((75000, 22000), (75000, 0))]
+
+    control_point = (intersection_calc(floating_line, threshold_lines[1]))
+    if control_point[0] > threshold_lines[1][1][0]:
+        intersection = (intersection_calc(floating_line, threshold_lines[2]))
+    elif control_point[1] > threshold_lines[0][0][1]:
+        intersection = (intersection_calc(floating_line, threshold_lines[0]))
+    else:
+        intersection = control_point
+    return intersection
+
+
 def graph_background_create():
     global graph_active
+    fl_h_int = 0
+    fl_c_int = 0
     if fls_active == 1:
         graph_75x100 = Figure(figsize=(6, 5), dpi=100)
         plot = graph_75x100.add_subplot(111)
-        plot.plot([0, 45, 100, 100], [75, 75, 30, 0], "-k")
-        plot.plot([0, 50], [0, 30], "-r")
+        plot.plot([0, 45000, 100000, 100000], [75000, 75000, 30000, 0], "-k")
+        if input_info:
+            fl_h_int, fl_c_int = intersection_point()
+            plot.plot([input_info[0], fl_h_int], [input_info[1], fl_c_int], "-r")
 
         graph_active = FigureCanvasTkAgg(graph_75x100, master=frame)
         graph_active.draw()
     else:
         graph_56x75 = Figure(figsize=(6, 5), dpi=100)
         plot = graph_56x75.add_subplot(111)
-        plot.plot([0, 33, 75, 75], [56, 56, 22, 0], "-k")
-        plot.plot([0, 50], [0, 30], "-r")
+        plot.plot([0, 33000, 75000, 75000], [56000, 56000, 22000, 0], "-k")
+        if input_info:
+            fl_h_int, fl_c_int = intersection_point()
+            plot.plot([input_info[0], fl_h_int], [input_info[1], fl_c_int], "-r")
 
         graph_active = FigureCanvasTkAgg(graph_56x75, master=frame)
         graph_active.draw()
     graph_active.get_tk_widget().grid(row=0, column=0, padx=20, pady=20)
+    display_threshold(fl_h_int, fl_c_int)
+
+
+def display_threshold(fh, fc):
+    fh = str(round(fh, 2))
+    fc = str(round(fc, 2))
+    threshold = Label(frame, text=fh + " FH " + fc + " FC")
+    threshold.grid(row=1, column=1)
 
 
 def graph_switch():
